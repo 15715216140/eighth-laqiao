873648bde5baf0b4fb816785ccc450158320e8a8
xminjie <xmj15715216140@163.com>
2018-10-11 21:06:54 +0800

    -

--- 蓝桥/ADV/ADV-238.cpp
+++ 蓝桥/ADV/ADV-238.cpp
@@ -8,20 +8,10 @@ int main() {
     if(ans == 0) {
         printf("0.000000E+000");
     } else if (ans >= 1){
-        long long num = 0;
-        double t = ans;
-        while((int)(t / 10)) {
-            num++;
-            t /= 10;
-        }
+        int num = log10(ans);
         printf("%6fE+%03lld",ans/pow(10,num),23+num);
     }else{
-        long long num = 0;
-        double t = ans;
-        while((int)t == 0) {
-            num++;
-            t *= 10;
-        }
+        int num = log10(ans)*-1 + 1;
         printf("%6fE+%03lld",ans*pow(10,num),23-num);
     }
     return 0;
